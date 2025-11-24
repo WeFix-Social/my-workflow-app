d0f93c32e2b37499f73dec264717b2099595a9d5
Eugen Martin <eugeniusmartinus@googlemail.com>
2025-11-24 09:30:46 +0100

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,32 @@
+// import adapter from '@sveltejs/adapter-auto';
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess({}),
+
+  compilerOptions: {
+    // runes: true,
+  },
 
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    csrf: {
+      // TODO: revisit this - we should only trust origins that are explicitly allowed
+      trustedOrigins: ["*"], // handle in hooks.server.ts
+    },
+    version: {
+      // Force reload when assets change
+      name: Date.now().toString(),
+      // poll for changes every second
+      pollInterval: 10_000,
+    },
   },
 };
 
